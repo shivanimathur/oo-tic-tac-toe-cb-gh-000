0de3f7372fface886d6f659409a990bd798d6fa6
Shivani Mathur <qwellery@gmail.com>
2017-12-27 06:41:42 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,7 +2,7 @@ class TicTacToe
   def initialize
     @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
-  def @WIN_COMBINATIONS = [
+  WIN_COMBINATIONS = [
     [0,1,2],
     [3,4,5],
     [6,7,8],
@@ -12,5 +12,5 @@ class TicTacToe
     [0,4,8],
     [6,4,2]
   ]
-end
+
 end
