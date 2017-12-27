36d0c86618b7e25a491123ed4dc5ff3d5bdda348
Shivani Mathur <qwellery@gmail.com>
2017-12-27 06:53:16 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,6 +15,10 @@ class TicTacToe
 
   def display_board=(board)
     @board = board
+  end
+
+  def display_board(board)
+    @board = board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
